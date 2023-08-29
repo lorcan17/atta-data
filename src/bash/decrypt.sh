e22d6ddae6a7bba3ffcfb31e5e416c9dad73bdb7
Lorcan Travers <ltravers92@gmail.com>
2023-08-29 16:44:46 -0700

    decrypt credentials function

---
+++ src/bash/decrypt.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Decrypt the file
+# mkdir $HOME/
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPT_KEY" \
+--output config/gcloud_service_account.json config/gcloud_service_account.json.gpg
