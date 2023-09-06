ee02921b6b7923598a9ffdf3ea9b7abbb1dbc392
Lorcan Travers <ltravers92@gmail.com>
2023-09-05 21:32:09 -0700

    Make file executable

--- src/bash/decrypt.sh
+++ src/bash/decrypt.sh
@@ -4,5 +4,6 @@
 # mkdir $HOME/
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
+# git update-index --chmod=+x src/bash/decrypt.sh 
 gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPT_KEY" \
 --output config/gcloud_service_account.json config/gcloud_service_account.json.gpg
